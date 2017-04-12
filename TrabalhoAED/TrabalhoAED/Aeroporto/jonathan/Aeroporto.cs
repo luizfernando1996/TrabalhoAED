4392ad1e8ac614c18053b12dd420066567f75d56
Jonathan Dias <jonathandiasr@outlook.com.br>
2017-04-11 22:27:55 -0300

    implentando aeropoto

---
+++ TrabalhoAED/TrabalhoAED/Aeroporto/jonathan/Aeroporto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoAED.Aeroporto.jonathan
+{
+    class Aeroporto
+    {
+        public void criarAeroporto(string cidade, string codigo)
+        {
+
+        } 
+    }
+}
---
+++ TrabalhoAED/TrabalhoAED/Aeroporto/jonathan/NodeAeroporto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoAED.Aeroporto.jonathan
+{
+    class NodeAeroporto
+    {
+        public string cidade;
+        public string sigla;
+
+        public  NodeAeroporto(string cidade, string sigla )
+        {
+            this.cidade = cidade;
+            this.sigla = sigla;
+        }
+    }
+}
